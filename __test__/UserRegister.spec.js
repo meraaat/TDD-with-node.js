fee2ec0d29e98a7fe86c45c603291f2560aaf507
merat <mera1takbari@gmail.com>
2024-03-27 01:04:43 +0330

    test : userRegister creation spec

--- __test__/UserRegister.spec.js
+++ __test__/UserRegister.spec.js
@@ -1,13 +1,32 @@
 const request = require('supertest');
-const app = require('../app');
+const app = require('../src/app');
 
-it('returns 200 OK when signup request is valid', () => {
-  request(app)
-    .post('/app/1.0/users')
-    .send({
-      username: 'user1',
-      email: 'user1@gmail.com',
-      password: 'P4ssword',
-    })
-    .expect(200);
+describe('User Registration', () => {
+  it('returns 200 OK when signup request is valid', (done) => {
+    request(app)
+      .post('/app/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.status).toBe(200);
+        done();
+      });
+  });
+
+  it('returns success message when signup request is valid', (done) => {
+    request(app)
+      .post('/app/1.0/users')
+      .send({
+        username: 'user1',
+        email: 'user1@gmail.com',
+        password: 'P4ssword',
+      })
+      .then((response) => {
+        expect(response.body.message).toBe('User created');
+        done();
+      });
+  });
 });
