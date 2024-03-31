898d2e9c5474f132123cf627bc058aa2271757e8
merat <mera1takbari@gmail.com>
2024-03-31 19:24:53 +0330

    refactor : add UserService layer for keeping user functionalities isolated

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -1,15 +1,10 @@
-const bcrypt = require('bcrypt');
-const User = require('./User');
+const UserService = require('./UserService');
 
 const express = require('express');
 const router = express.Router();
 
 router.post('/api/1.0/users', async (req, res) => {
-  const hash = await bcrypt.hash(req.body.password, 10);
-  const user = { ...req.body, password: hash };
-
-  await User.create(user);
-
+  await UserService.save(req.body);
   return res.send({ message: 'User created' });
 });
 
