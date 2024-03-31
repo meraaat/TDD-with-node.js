69d76fbf801fa7c995fd47a8addb4c75e4508f5c
merat <mera1takbari@gmail.com>
2024-03-31 18:13:46 +0330

    feat : implement bcrypt functionality to hashes the user password for saving in DB

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
-
+const bcrypt = require('bcrypt');
 const User = require('../src/user/User');
 
+const app = express();
+
+app.use(express.json());
+
 app.post('/api/1.0/users', (req, res) => {
-  User.create(req.body).then(() => {
-    return res.send({ message: 'User created' });
+  bcrypt.hash(req.body.password, 10).then((hash) => {
+    const user = { ...req.body, password: hash };
+    User.create(user).then(() => {
+      return res.send({ message: 'User created' });
+    });
   });
 });
 
