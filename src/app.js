fa6c6530c458ec446eb0fa64e4d90c8c590f0440
merat <mera1takbari@gmail.com>
2024-03-31 13:55:03 +0330

    feat : add creation functionality in user create post request

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
+const User = require('../src/user/User');
+
 app.post('/app/1.0/users', (req, res) => {
-  return res.send({ message: 'User created' });
+  User.create(req.body).then(() => {
+    return res.send({ message: 'User created' });
+  });
 });
 
 module.exports = app;
