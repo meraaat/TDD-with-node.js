4f8abaa242e9fd6013374215b4bc113c868a842a
merat <mera1takbari@gmail.com>
2024-03-31 19:14:04 +0330

    refactor : move user rest api functionalities to UserRouter file

--- src/app.js
+++ src/app.js
@@ -1,18 +1,10 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
-const User = require('../src/user/User');
+const userRouter = require('./user/UserRouter');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/api/1.0/users', (req, res) => {
-  bcrypt.hash(req.body.password, 10).then((hash) => {
-    const user = { ...req.body, password: hash };
-    User.create(user).then(() => {
-      return res.send({ message: 'User created' });
-    });
-  });
-});
+app.use(userRouter);
 
 module.exports = app;
