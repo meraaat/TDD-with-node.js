a2b22e76ea1ef31248dee5d2b36e2e3689eae796
merat <mera1takbari@gmail.com>
2024-03-31 17:20:59 +0330

    feat : add express json parser and update the user creation post request address

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 
 const User = require('../src/user/User');
 
-app.post('/app/1.0/users', (req, res) => {
+app.post('/api/1.0/users', (req, res) => {
   User.create(req.body).then(() => {
     return res.send({ message: 'User created' });
   });
