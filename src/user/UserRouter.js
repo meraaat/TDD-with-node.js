1d10bd139835c5a47cf9b19266eb460f4270cc3c
merat <mera1takbari@gmail.com>
2024-04-01 17:22:57 +0330

    feat : add validation for input user

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -4,6 +4,12 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/api/1.0/users', async (req, res) => {
+  const user = req.body;
+
+  if (user.username === null) {
+    return res.status(400).send({ validationError: {} });
+  }
+
   await UserService.save(req.body);
   return res.send({ message: 'User created' });
 });
