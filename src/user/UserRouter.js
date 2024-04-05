b3bcc7a36a1deb8cdb6b5865db0816263bfef198
merat <mera1takbari@gmail.com>
2024-04-05 18:48:32 +0330

    feat : update validation messages with translation json file for internationalization

--- src/user/UserRouter.js
+++ src/user/UserRouter.js
@@ -6,38 +6,32 @@ const router = express.Router();
 
 router.post(
   '/api/1.0/users',
-  check('username')
-    .notEmpty()
-    .withMessage('username cannot be null')
-    .bail()
-    .isLength({ min: 4, max: 32 })
-    .withMessage('username must min 4 and max 32 characters'),
+  check('username').notEmpty().withMessage('username_null').bail().isLength({ min: 4, max: 32 }).withMessage('username_size'),
   check('email')
     .notEmpty()
-    .withMessage('E-mail cannot be null')
+    .withMessage('email_null')
     .bail()
     .isEmail()
-    .withMessage('E-mail is not valid')
+    .withMessage('email_invalid')
     .bail()
     .custom(async (email) => {
       const user = await UserService.findByEmail(email);
-      if (user) throw new Error('E-mail is in use');
+      if (user) throw new Error('email_inuse');
     }),
   check('password')
     .notEmpty()
-    .withMessage('password cannot be null')
+    .withMessage('password_null')
     .bail()
     .isLength({ min: 6 })
-    .withMessage('password must be at least 6 characters')
+    .withMessage('password_size')
     .bail()
     .matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).*$/)
-    .withMessage('password must have at least 1 uppercase, 1 lowercase letter and 1 number'),
+    .withMessage('password_pattern'),
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       const validationError = {};
-      errors.array().forEach((error) => (validationError[error.path] = error.msg));
-      console.log(`errrrrror : ${Object.keys(validationError)}, ${Object.values(validationError)}`);
+      errors.array().forEach((error) => (validationError[error.path] = req.t(error.msg)));
       return res.status(400).send({ validationError });
     }
 
