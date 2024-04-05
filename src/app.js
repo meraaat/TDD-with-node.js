3bb245f14d62365f4e866711852378ce73de786c
merat <mera1takbari@gmail.com>
2024-04-05 17:49:00 +0330

    feat : add i18next configuration for international language detection

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const userRouter = require('./user/UserRouter');
+const i18next = require('i18next');
+const backend = require('i18next-fs-backend');
+const middleware = require('i18next-http-middleware');
+
+i18next
+  .use(backend)
+  .use(middleware.LanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    lng: 'en',
+    ns: ['translation'],
+    defaultNS: 'translation',
+    backend: {
+      loadPath: './locales/{{lng}}/{{ns}}.json',
+    },
+    detection: {
+      lookupHeader: 'accept-language',
+    },
+  });
 
 const app = express();
+
+app.use(middleware.handle(i18next));
 
 app.use(express.json());
 
