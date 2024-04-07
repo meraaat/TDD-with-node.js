4e4db10539384d792e1d5035a9a54f13587e3332
merat <mera1takbari@gmail.com>
2024-04-07 16:19:41 +0330

    feat : add generate token functionality for passing activationToken in user creation func

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -1,10 +1,15 @@
 const bcrypt = require('bcrypt');
 const User = require('./User');
+const crypto = require('crypto');
+
+const generateToken = (length) => {
+  return crypto.randomBytes(length).toString('hex').substring(0, length);
+};
 
 const save = async (body) => {
   const { username, email, password } = body;
   const hash = await bcrypt.hash(password, 10);
-  const user = { username, email, password: hash };
+  const user = { username, email, password: hash, activationToken: generateToken(16) };
 
   await User.create(user);
 };
