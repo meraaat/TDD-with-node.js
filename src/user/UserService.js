74b9e680ed2fed1d948e10bfb8e744a96b481b1e
merat <mera1takbari@gmail.com>
2024-04-08 17:33:21 +0330

    feat : implement mailing functionalities for sending activationToken to user

--- src/user/UserService.js
+++ src/user/UserService.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 const User = require('./User');
 const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const nodemailerStub = require('nodemailer-stub');
 
 const generateToken = (length) => {
   return crypto.randomBytes(length).toString('hex').substring(0, length);
@@ -10,6 +12,14 @@ const save = async (body) => {
   const { username, email, password } = body;
   const hash = await bcrypt.hash(password, 10);
   const user = { username, email, password: hash, activationToken: generateToken(16) };
+
+  const transporter = nodemailer.createTransport(nodemailerStub.stubTransport);
+  await transporter.sendMail({
+    from: 'MerAt App <info@merat-app.com>',
+    to: email,
+    subject: 'Account Activation',
+    html: `Token is ${user.activationToken}`,
+  });
 
   await User.create(user);
 };
